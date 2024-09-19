1e7e3885b7ee258a295d922ecf90772b32795f7d
Heriberto Velázquez <velazquezhery@gmail.com>
2024-09-18 19:06:53 -0600

    OperasBas "18/09/24"

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProducto } from '../producto';
 
 @Component({
   selector: 'app-product-list',
@@ -10,19 +11,19 @@ export class ProductListComponent {
   imageWidth:number=50;
   imageMargi:number=2;
   muestraImg:boolean=true;
-  listFilter:String='';
+  listFilter:string='';
 
   showImage():void{
     this.muestraImg=!this.muestraImg;
   }
 
-  productos:any[]=[
+  productos:IProducto[]=[
     {
       "productoID":1,
       "Modelo":'A4',
       "Descripcion":"2 puertas familiar",
       "year":"Marzo 30 2024",
-      "Precio":"500,000",
+      "Precio": 500000,
       "Marca":"AUDI",
       "Color":"Negro",
       "imagenUrl":"https://wallpapercave.com/wp/wp1849317.jpg"
@@ -32,7 +33,7 @@ export class ProductListComponent {
       "Modelo":"Mazda 3",
       "Descripcion":"4 puertas familiar",
       "year":"Diciembre 25 2024",
-      "Precio":"300,000",
+      "Precio":300000,
       "Marca":"KIA",
       "Color":"Plata",
       "imagenUrl":"https://media-assets.mazda.eu/image/upload/q_auto,f_auto/mazdaes/globalassets/cars/2024-mazda3-hatchback/showroom/230307_673_378_slidinggallerystatic_large_m3.jpg?rnd=498ba7"
@@ -42,7 +43,7 @@ export class ProductListComponent {
       "Modelo":"GOLF",
       "Descripcion":"2 puertas familiar",
       "year":"Enero 3 2024",
-      "Precio":"400,000",
+      "Precio":400000,
       "Marca":"VOLKSWAGEN",
       "Color":"ROJO",
       "imagenUrl":"https://img.remediosdigitales.com/5d0856/golf-gti/1366_2000.jpeg"
